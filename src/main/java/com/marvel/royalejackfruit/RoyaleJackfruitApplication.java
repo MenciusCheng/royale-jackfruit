39fe9e252cc808311d924131d5bd49dd63c34ea4
程孟威 <295567172@qq.com>
2019-11-08 18:38:24 +0800

    init

---
+++ src/main/java/com/marvel/royalejackfruit/RoyaleJackfruitApplication.java
@@ -0,0 +1,12 @@
+package com.marvel.royalejackfruit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoyaleJackfruitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoyaleJackfruitApplication.class, args);
+	}
+}
